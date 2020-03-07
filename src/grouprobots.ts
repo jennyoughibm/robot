9af1440e89c682b48ad1aa9a50cdffeda0067aaf
jenny <jenny.ough@ibm.com>
2020-03-07 12:21:18 +0000

    update on grouprobots:

--- src/grouprobots.ts
+++ src/grouprobots.ts
@@ -3,10 +3,10 @@ import Movement from "./movement";
 import {Pos} from "./Pos";
 
 let groupRobotPos: Pos[] = [];
-export const destForGroupRobots = (startMove : Movement, ) : Pos[] => {
+export const destForGroupRobots = (startMove : Movement[], ) : Pos[] => {
     let i;
-    for (i = 0; i < 500; i++) {
-       const robotpos :Pos = moveToDest(startMove);
+    for (i = 0; i < startMove.length; i++) {
+       const robotpos :Pos = moveToDest(startMove[i]);
         groupRobotPos.push(robotpos);
     }
     return groupRobotPos;
